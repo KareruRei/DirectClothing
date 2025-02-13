429185ac5b1efe2087c00aaf58330fe82d81e5c5
Jon Doe <Jon-Doe404>
2025-02-13 09:47:27 +0800

    commit

--- general/Address.java
+++ general/Address.java
@@ -4,7 +4,7 @@ final public class Address {
     private String addressLine1;
 	private String addressLine2;
 	private String city;
-	private int zipCode;efwef
+	private int zipCode;efwefvfbbfxbf
 	private String state;
 	private String count;
 
