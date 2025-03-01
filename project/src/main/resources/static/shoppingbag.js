a6596348b1f124782e15746bc6d29398c75901a5
gabbrealz <christianagot11@gmail.com>
2025-03-01 23:27:05 +0800

    added additional functionality to shoppingbag

--- project/src/main/resources/static/shoppingbag.js
+++ project/src/main/resources/static/shoppingbag.js
@@ -13,6 +13,9 @@ window.onload = function() {
         // cardChildren[1].children[1].id = "quantity_id"+cardID;
         // cardChildren[1].children[2].id = "addButton_id"+cardID;
 
+        var qtyNum = document.getElementById('quantity_id' + cardID);
+        qtyNum.addEventListener("input", function() {inputQty(card.id, qtyNum.value);});
+
         document.getElementById("subButton_id"+cardID).addEventListener("click", function() {changeQty(card.id, -1);});
         document.getElementById("addButton_id"+cardID).addEventListener("click", function() {changeQty(card.id, 1);});
         document.getElementById("removeButton_id"+cardID).addEventListener("click", function() {toggleItemRemoval(card.id);});
@@ -33,6 +36,50 @@ function changeQty(itemID, change) {
 
     var newQty = Number(qtyNum.value) - change;
     if (newQty < 1) newQty = 1;
+    
+    fetch("/change-qty", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ itemID: itemID, newQty: newQty })
+    })
+    .then(response => {
+        if (response.status === 500) {
+            throw new Error("Failed to change quantity. Not enough stock!");
+        } else {
+            return response.json();
+        }
+    })
+    .then(cartUpdate => {
+        qtyNum.value = cartUpdate.newQty;
+        discountedPrice.textContent = "PHP " + cartUpdate.discountedPrice;
+
+        orderLineQty.textContent = "Qty. " + cartUpdate.newQty;
+        orderLinePrice.textContent = "PHP " + cartUpdate.discountedPrice;
+
+        total.textContent = "PHP " + cartUpdate.totalPrice;
+
+        if (rawPrice !== null) {
+            rawPrice.textContent = "PHP " + cartUpdate.rawPrice;
+        }
+    })
+    .catch(error => {
+        alert(error);
+    });
+}
+
+function inputQty(itemID, newQty) {
+    var qtyNum = document.getElementById('quantity_id' + itemID);
+    var rawPrice = document.getElementById('rawPrice_id' + itemID);
+    var discountedPrice = document.getElementById('discountedPrice_id' + itemID);
+    var orderLineQty = document.getElementById('orderLineQty_id' + itemID);
+    var orderLinePrice = document.getElementById('orderLinePrice_id' + itemID);
+    var total = document.getElementById('totalAmt');
+    
+    if (newQty < 1 || newQty === "") newQty = 1;
+
     
     fetch("/change-qty", {
         method: 'POST',
