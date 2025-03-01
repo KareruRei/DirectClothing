1172538b10eb006481c6f3430877ce3401a5eb34
Your Name <you@example.com>
2025-03-02 01:56:57 +0800

    commit

--- project/src/main/resources/static/shoppingbag.js
+++ project/src/main/resources/static/shoppingbag.js
@@ -63,3 +63,51 @@ function toggleItemRemoval(itemID) {
         item.addEventListener("transitionend", function() {document.getElementById(itemID).remove();});
     })
 }
+window.onclick = function(event) {
+    let cartUI = document.getElementById('cart-ui');
+    if (event.target === cartUI) {
+        closeCart();
+    }
+}
+
+function removeFromCart(itemID) {
+            
+    fetch("/item-removed", {
+        method: 'POST',
+        headers: {'Content-Type': 'text/plain',},
+        body: String(itemID)
+    })
+        .then(response => {
+            if (response.status === 500) alert("Item does not exist! Failed to remove item from cart.");
+            return response.text();
+        })
+        .then(cartSize => {
+            var cartContainer = document.getElementById('cart-items');
+
+            document.getElementById('cart-count').innerText = cartSize;
+            if (cartSize === "0") {
+                document.getElementById('cart-count').style.display = "none";
+                cartContainer.innerHTML = "<p>Cart is empty</p>";
+            }
+            
+            for (let cartItem of cartContainer.children) {
+                if (cartItem.id === itemID) {
+                    cartItem.remove();
+                    break;
+                }
+            }
+        })
+}
+
+function closeCart() {
+    document.getElementById('cart-ui').style.display = "none";
+}
+
+function openCart() {
+    var cartContainer = document.getElementById('cart-items');
+    if (cartContainer.children.length === 0) {
+        cartContainer.innerHTML = "<p>Cart is empty</p>";
+    }
+
+    document.getElementById('cart-ui').style.display = "flex";
+}
